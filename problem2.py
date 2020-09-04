c3bf06b6648265eb056b2cd951babbbe7fb05595
chensel196 <chensel2@illinois.edu>
2020-09-04 18:23:49 +0000

    Answered all parts to problem 2

--- problem2.py
+++ problem2.py
@@ -1,7 +1,16 @@
 ### PROBLEM 2
+ill = 7
+j = 2
+result = ill**2 + 3*j - 5
+print(result)
 
-def main():
-    print("hello world!")
+# answer to part a is 50
 
-if __name__ == "__main__":
-    main()
+## answer to part b is 50 
+
+### answer to part c is   File "problem2.py", line 15 \
+   ### 21i = 7
+   ###   ^
+### SyntaxError: invalid syntax ###
+
+#### answer to part d is 50

4b6f2f632b2d45b80c4f5d07c22ecdaf29a7aba0
chensel196 <chensel2@illinois.edu>
2020-09-04 18:31:41 +0000

    Finished problem 3 and therefore the whole assignment

--- problem3.py
+++ problem3.py
@@ -1,7 +1,7 @@
 ### PROBLEM 3
+degF = 40.5
+degC = 0.55 * (degF - 32)
+print(degC)
 
-def main():
-    print("hello world!")
-
-if __name__ == "__main__":
-    main()
+#when degF is 40, degC is 4.4
+##when degF is 40.5, degC is 4.675

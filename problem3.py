f50a88993c6acc0fc7cd97d025e40cdfbde1a076
chensel196 <chensel2@illinois.edu>
2020-09-04 18:41:51 +0000

    -changed one variable in problem 3 from 40 to -40 for degF -made formula for degC more precise; 0.55 to (5/9)

--- problem3.py
+++ problem3.py
@@ -1,7 +1,7 @@
 ### PROBLEM 3
 degF = 40.5
-degC = 0.55 * (degF - 32)
+degC = (5/9) * (degF - 32)
 print(degC)
 
-#when degF is 40, degC is 4.4
-##when degF is 40.5, degC is 4.675
+#when degF is -40.0, degC is -40.0
+##when degF is 40.5, degC is 4.72
